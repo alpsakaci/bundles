0b74b3712a10e88711235b7d9504957f86c3e6d8
Alp <erenalpsakaci@gmail.com>
2020-10-18 22:28:02 +0300

    basic note model created

--- floppy/models.py
+++ floppy/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=50, blank=True, null=True)
+    content = models.TextField()
