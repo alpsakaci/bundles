6a9b1ace0424a566aa0a24f83687337ac8fdc0c5
Alp <erenalpsakaci@gmail.com>
2020-10-19 22:30:34 +0300

    added list and retrieve methods to note viewset

--- floppy/views.py
+++ floppy/views.py
@@ -1,7 +1,29 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import routers, serializers, viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+
 from .models import Note
 from .serializers import NoteSerializer
 
 class NoteViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
+    authentication_classes = (SessionAuthentication, BasicAuthentication, TokenAuthentication)
+
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    def list(self, request):
+        queryset = Note.objects.filter(owner = request.user)
+        serializer = NoteSerializer(queryset, many = True)
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk = None):
+        queryset = Note.objects.filter(owner = request.user)
+        note = get_object_or_404(queryset, pk = pk)
+        serializer = NoteSerializer(note)
+
+        return Response(serializer.data)
