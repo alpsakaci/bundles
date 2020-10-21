37101acfbab305d2130005a130edf51554a2e746
Alp <erenalpsakaci@gmail.com>
2020-10-21 17:02:36 +0300

    date fields added to floppy

--- floppy/serializers.py
+++ floppy/serializers.py
@@ -3,6 +3,17 @@ from rest_framework.request import Request
 from .models import Note
 
 class NoteSerializer(serializers.HyperlinkedModelSerializer):
+
+    date_modified = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Note
-        fields = ['id', 'title', 'content']
+        fields = ['id', 'title', 'content', 'date_created', 'date_modified']
+
+    def create(self, validated_data):
+        user = self.context['request'].user  
+        validated_data['owner_id'] = user.id
+        note = super(NoteSerializer, self).create(validated_data)
+        note.save()
+
+        return note 
