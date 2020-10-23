25d30a1b3571a285355765a1ba8d14f4b66acb1f
Alp <erenalpsakaci@gmail.com>
2020-10-23 13:13:52 +0300

    read secret key from file

--- bundles/settings.py
+++ bundles/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g=qh+fmtwsmtpojr6@8&m4y7d5-np6(-jjj*rq4&2a5@b2p&zp'
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
