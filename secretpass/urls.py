174f597cd738f6e1a99369f2b889b323f2c7cacf
Alp <alpsakaci@protonmail.com>
2020-12-02 23:04:56 +0300

    edit account form

--- secretpass/urls.py
+++ secretpass/urls.py
@@ -7,10 +7,11 @@ router.register(r"users", views.UserViewSet)
 router.register(r"accounts", views.AccountViewSet)
 
 urlpatterns = [
-    path('', webviews.index, name="spindex"),
-    path('create/', webviews.create, name="spcreate"),
-    path('api/accounts/search/', views.search_account),
-    path('api/generate_password/', views.generate_password),
+    path("", webviews.index, name="spindex"),
+    path("create/", webviews.create, name="spcreate"),
+    path("edit/<int:acc_id>/", webviews.edit, name="spedit"),
+    path("api/accounts/search/", views.search_account),
+    path("api/generate_password/", views.generate_password),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
 ]
--- secretpass/webviews.py
+++ secretpass/webviews.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Account
-from .forms import AccountForm
+from .forms import AccountForm, AccountUpdateForm
 from .views import AccountViewSet
 from .serializers import AccountSerializer
+from .crypto import encrypt_password, decrypt_password
 
 
 @login_required(login_url="/admin/login")
@@ -42,3 +43,46 @@ def create(request):
         context = {"form": form}
 
     return render(request, "secretpass/create.html", context)
+
+
+@login_required(login_url="/admin/login")
+def edit(request, acc_id):
+    if request.method == "POST":
+        form = AccountUpdateForm(request.POST)
+
+        if form.is_valid():
+            service = form.cleaned_data["service"]
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            repeat = form.cleaned_data["repeat"]
+            use_current_password = form.cleaned_data["use_current_password"]
+
+            acc = get_object_or_404(
+                Account.objects.filter(id=acc_id, owner=request.user)
+            )
+            if use_current_password == True:
+                acc.service = service
+                acc.username = username
+                acc.save()
+            elif password != "" and password.__eq__(repeat):
+                acc.service = service
+                acc.username = username
+                acc.password = encrypt_password(password)
+                acc.save()
+            else:
+                context = {"form": form}
+                form.add_error("password", "Password does not match.")
+                return render(request, "secretpass/edit.html", context)
+
+            return redirect(index)
+    else:
+        account = get_object_or_404(
+            Account.objects.filter(id=acc_id, owner=request.user)
+        )
+        password = decrypt_password(account.password)
+        form = AccountUpdateForm(
+            initial={"service": account.service, "username": account.username}
+        )
+        context = {"form": form}
+
+    return render(request, "secretpass/edit.html", context)
